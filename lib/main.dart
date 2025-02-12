90fc2773ab5fc15724276f8292c7f5954cc09ea8
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-12 16:25:15 +0200

    link project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lask_news_app/core/utils/app_routes.dart';
 import 'package:lask_news_app/core/utils/routes.dart';
+import 'package:lask_news_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
