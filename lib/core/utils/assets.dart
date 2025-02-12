577b932dae6b31af8df14a2174a12b01ab910b8e
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-12 10:12:27 +0200

    setup fonts and styles

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBookmark
+  /// assets/images/bookmark.svg
+  static const String imagesBookmark = "assets/images/bookmark.svg";
+
+  /// Assets for imagesExplore
+  /// assets/images/explore.svg
+  static const String imagesExplore = "assets/images/explore.svg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+
+  /// Assets for imagesUser
+  /// assets/images/user.svg
+  static const String imagesUser = "assets/images/user.svg";
+}
+
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,67 @@
+class Styles {
+  static TextStyle regular12 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle semiBold12 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle regular14 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle semiBold14 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold16 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold18 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold20 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold24 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold32 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle bold40 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle bold48 = const TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+  );
+}
